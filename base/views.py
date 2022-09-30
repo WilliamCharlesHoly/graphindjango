d9c524fe80e1869e647de86b0245175b92aa2f87
WilliamCharlesHoly <92197884+WilliamCharlesHoly@users.noreply.github.com>
2022-09-30 13:23:40 +0800

    Update views.py

--- base/views.py
+++ base/views.py
@@ -3,6 +3,51 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    connection = mysql.connector.connect(host='localhost',
+                                         database='eurosoccerleague',
+                                         user='root',
+                                         password='Mysql12345678')
 
+    players_height = pd.read_sql("""SELECT CASE
+                                    WHEN ROUND(height)<165 then 165
+                                    WHEN ROUND(height)>195 then 195
+                                    ELSE ROUND(height)
+                                    END AS calc_height, 
+                                    COUNT(height) AS distribution, 
+                                    (avg(PA_Grouped.avg_overall_rating)) AS avg_overall_rating,
+                                    (avg(PA_Grouped.avg_potential)) AS avg_potential,
+                                    AVG(weight) AS avg_weight 
+                                    FROM player
+                                    LEFT JOIN (SELECT player_attributes.player_api_id, 
+                                    avg(player_attributes.overall_rating) AS avg_overall_rating,
+                                    avg(player_attributes.potential) AS avg_potential  
+                                    FROM player_attributes
+                                    GROUP BY player_attributes.player_api_id) 
+                                    AS PA_Grouped ON player.player_api_id = PA_Grouped.player_api_id
+                                    GROUP BY calc_height
+                                    ORDER BY calc_height;""", connection)
+    
+    df = pd.DataFrame(players_height)
 
-    return render(request, 'base/home.html')
+  
+    # line plot for math marks
+    df.plot(kind = 'line',
+        x = 'calc_height',
+        y = 'avg_overall_rating',
+        color = 'green', figsize=(12,5))
+  
+    # set the title
+    plt.title('Potential vs Height')
+  
+    # show the plot
+    # plt.show()
+
+    plt.savefig('static/images/my_plot.png')
+
+    # parsing the DataFrame in json format.
+    json_records = df.reset_index().to_json(orient ='records')
+    data = []
+    data = json.loads(json_records)
+    context = {'d': data}
+
+    return render(request, 'base/home.html', context)
